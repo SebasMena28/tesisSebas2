3999dafa2cd499fb00e21819d2585c96a5aff5bf
sebas <78992252+SebasMena28@users.noreply.github.com>
2022-10-01 19:53:43 -0500

    cambio de base y funciona
    haciendo el listar pacientes

--- src/claves.js
+++ src/claves.js
@@ -3,7 +3,8 @@ module.exports = {
     database: {
         host: 'localhost',
         user: 'root',
-        password: '1234',
-        database: 'crudjs'
+        password: '',
+        database: 'crudjs',
+        port: 3306
     }
 }
--- src/routes/pacientes.js
+++ src/routes/pacientes.js
@@ -8,23 +8,35 @@ router.get('/nuevoPaciente', (req, res) => {
     res.render('pacientes/nuevoPaciente'); 
 })
 
-router.post('/nuevoPaciente', (req, res)=>{
+router.post('/nuevoPaciente', async (req, res)=>{
     res.send('funciona xd');
     console.log(req.body);
     const {cedula, apellidoPaterno, apellidoMaterno, primerNombre, segundoNombre, fechaNacimiento, genero, estadoCivil, ocupacion, religion} = req.body;
     const nuevoPaciente = {
         cedula: "hola",
-        apellidoPaterno,
-        apellidoMaterno, 
-        primerNombre, 
-        segundoNombre, 
-        fechaNacimiento, 
-        genero,
-        estadoCivil,
-        ocupacion,
-        religion
+        apellidoPaterno: "asd",
+        apellidoMaterno : "asd", 
+        primerNombre : "asd", 
+        segundoNombre : "asd", 
+        fechaNacimiento : "asd", 
+        genero : "asd",
+        estadoCivil : "asd",
+        ocupacion : "asd",
+        religion: "asd"
     }; //no se porque chuchas no funciona
+
+    //await es porque es una funcion asincrona
+    await pool.query('INSERT INTO PACIENTES set ?', [nuevoPaciente])
+    
     console.log(nuevoPaciente);
+});
+
+
+router.get('/', async (req, res) => {
+    const pacientes = await pool.query('SELECT * FROM PACIENTES');
+    //console.log(pacientes);
+    res.render('pacientes/lista', {pacientes}); //renderizando y mando los pacientes registrados
 })
+
 
 module.exports = router;
