f27c32c962f7aadab3590f04bf317de22a3ef123
sebas <78992252+SebasMena28@users.noreply.github.com>
2022-12-12 16:14:04 -0500

    ya vale el login

--- src/routes/autenticacion.js
+++ src/routes/autenticacion.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport')
+const pool = require('../basedatos');
 
 router.get('/registro', (req, res) =>{
     res.render('logeo/inicioSesion');
+});
+
+router.post('/autenticacion/inicio', async  (req, res) =>{
+    //const { usuario, contrasenia} = req.params;
+
+    const iniciar = {
+        usuario : req.body.usuario, 
+        contrasenia : req.body.contrasenia
+    }
+
+    const existe = await pool.query('SELECT * FROM USUARIOS WHERE NOMBREUSUARIO = ? AND PASS = ?;', [iniciar.usuario, iniciar.contrasenia]);
+
+    //console.log(existe);
+    if (existe != '') {
+        res.render('layouts/main',{ iniciar });
+    }
+    else{
+        res.render('logeo/inicioSesion');
+        console.log('no hay, no existe');
+    }
+});
+
+router.get('/autenticacion/registro', (req, res) =>{
+    res.render('logeo/registro');
 })
 
-router.post('/autenticacion/inicio', (req, res) =>{
+router.post('/autenticacion/registro', (req, res) =>{
     res.render('layouts/main');
 })
 
@@ -20,11 +45,11 @@ router.post('/autenticacion/inicio', (req, res) =>{
     res.send('recibido siiiiiiiuuuuuuuuuuuu');
 })*/
 
-router.post('/registro', passport.authenticate('registro', {
+/*router.post('/registro', passport.authenticate('registro', {
         successRedirect: '/perfil',
         failureRedirect: '/registro',
         failureFlash: true
-}));
+}));*/
 
 router.get('/perfil', (req, res) => {
     res.send('si se pudo burro')
